d1f4ed16d35e963f42ac50fc2054303742e4aa6a
Barry <121305421@umail.ucc.ie>
2023-12-03 21:25:01 +0000

    Implemented UPDATED Code for ColourTable CLASS based on CURRENT test cases.
    TESTS (4) PASS

--- src/main/java/org/example/colourtable/ColourTable.java
+++ src/main/java/org/example/colourtable/ColourTable.java
@@ -1,20 +1,41 @@
 package org.example.colourtable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ColourTable {
-    private int[] palette;
+    private final int[] palette;
     private int size;
+    private final Set<Integer> colors;
 
     public ColourTable(int paletteSize) {
-
+        if (!isValidPaletteSize(paletteSize)) {
+            throw new IllegalArgumentException("Invalid palette size");
+        }
+        this.palette = new int[paletteSize];
+        this.size = 0;
+        this.colors = new HashSet<>();
     }
 
-    public void add(int color) {
-
+    public void add(int colour) {
+        if (size == palette.length) {
+            throw new IllegalStateException("Exceeding capacity of the ColourTable");
+        }
+        if (colors.contains(colour)) {
+            return;
+        }
+        palette[size] = colour;
+        size++;
+        colors.add(colour);
     }
 
     public int getSize() {
-
         return size;
     }
+
+    private boolean isValidPaletteSize(int paletteSize) {
+        return (paletteSize > 1 && paletteSize < 1025 && (paletteSize & (paletteSize - 1)) == 0);
+    }
 }
+
 
