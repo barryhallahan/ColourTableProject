65deb5a0bb79ede1c9da6b52e8e68d194e2ad164
Barry <121305421@umail.ucc.ie>
2023-12-03 20:28:18 +0000

    Implemented Initial Code for ColourTable CLASS based on CURRENT test cases.

---
+++ src/main/java/org/example/colourtable/ColourTable.java
@@ -0,0 +1,20 @@
+package org.example.colourtable;
+
+public class ColourTable {
+    private int[] palette;
+    private int size;
+
+    public ColourTable(int paletteSize) {
+
+    }
+
+    public void add(int color) {
+
+    }
+
+    public int getSize() {
+
+        return size;
+    }
+}
+
