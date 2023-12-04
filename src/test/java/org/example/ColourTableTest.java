181cf6efc1bf88b233cb85a9c84eb3ac392c35e9
Barry <121305421@umail.ucc.ie>
2023-12-04 12:34:53 +0000

    Updated Comments & JavaDOC,
    CODE READABILITY, UNDERSTANDABILITY & MAINTENANCE

--- src/test/java/org/example/ColourTableTest.java
+++ src/test/java/org/example/ColourTableTest.java
@@ -13,7 +13,11 @@ public class ColourTableTest {
      */
     @Test
     public void testValidPaletteSize() {
+
+        // Define VALID palette size (per specification)
         int validPaletteSize = 8;
+
+        // Verification, w/o throwing an exception
         Assertions.assertDoesNotThrow(() -> {
             new ColourTable(validPaletteSize);
         });
@@ -24,7 +28,11 @@ public class ColourTableTest {
      */
     @Test
     public void testInvalidPaletteSize() {
+
+        // Array of INVALID palette sizes to TEST
         int[] invalidPaletteSizes = {3, 10, 1025};
+
+        // LOOP ARRAY THROW EXCEPTION FOR EACH
         for (int paletteSize : invalidPaletteSizes) {
             Assertions.assertThrows(IllegalArgumentException.class, () -> new ColourTable(paletteSize));
         }
@@ -51,7 +59,8 @@ public class ColourTableTest {
      */
     @Test
     public void testExceedingPaletteCapacity() {
-        ColourTable colourTable = new ColourTable(2); // Palette size of 2
+        // Palette SIZE 2 -> VALID
+        ColourTable colourTable = new ColourTable(2);
 
         // Adding colours to FILL palette CAPACITY
         colourTable.add(0xFF0000);
@@ -61,19 +70,34 @@ public class ColourTableTest {
         Assertions.assertThrows(IllegalStateException.class, () -> colourTable.add(0x0000FF));
     }
 
+    /**
+     * Tests adding a VALID RGB value to the ColourTable palette.
+     *
+     * Ensures the ColourTable ACCEPTS & STORES VALID RGB value.
+     */
     @Test
     public void testValidRGBValue() {
+        // Palette size 4 -> VALID
         ColourTable colourTable = new ColourTable(4);
 
+        // ADD Red to ColourTable -> VALID
         colourTable.add(0xFF0000);
 
+        // Verify SIZE after ADDITION of colour
         Assertions.assertEquals(1, colourTable.getSize());
     }
 
+    /**
+     * Tests adding an INVALID RGB value to the ColourTable palette.
+     *
+     * Ensures the ColourTable rejects INVALID RGB value.
+     */
     @Test
     public void testInvalidRGBValue() {
+        // Palette size 4 -> VALID
         ColourTable colourTable = new ColourTable(4);
 
+        // ADD INVALID RGB colour, THROW EXCEPTION -> IllegalArgumentException
         Assertions.assertThrows(IllegalArgumentException.class, () -> colourTable.add(0x7654321));
     }
 }
